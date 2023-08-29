65bcabc873409c12d932deccd151fd24df33399b
thegreatestgiant <seanalter1@gmail.com>
2023-08-28 23:21:06 -0400

    Passed second test

--- crawl.js
+++ crawl.js
@@ -5,27 +5,18 @@ function getURLsFromHTML(htmlBody, baseURL) {
     const dom = new JSDOM(htmlBody)
     let arr = []
     let arr_of_anchor = dom.window.document.querySelectorAll('a')
-    for (a of arr_of_anchor) {
+    for (let a of arr_of_anchor) {
         let href = a.getAttribute('href');
-        if (href && href.startsWith('./')) {
-            href = href.split('/').slice(0, -1).join('/');
-        }
+        href = (href && href.startsWith('./')) ? href.split('').slice(1).join('') : href        
         try {
-            let url = new URL(a)
-            console.log(url)
-            arr.push(url.href)
+            var url = new URL(href);
         } catch (e) {
-            let url = new URL(baseURL + a)
-            console.log(url)
-            if(url.hostname.endsWith('.')){
-                console.log(url.hostname)
-                console.log(url)
-            }
-            arr.push(url.href)
+            var url = new URL(href, baseURL)
         }
-    }
-    console.log(arr)
+        arr.push(url.href)
 
+    }
+    return arr
 }
 
 
@@ -33,24 +24,6 @@ function normalizeURL(str){
     const url = new URL(str)
     return url.hostname + url.pathname
 }
-getURLsFromHTML(`<a href="./new_page.html">`,'https://test.me')
-// getURLsFromHTML(`<!DOCTYPE html>
-//     <html lang="en">
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <title>Test</title>
-//     </head>
-//     <body>
-//         <button type="submit" onsubmit="submit()">Click Me!</button>
-    
-//         <script>
-//             const submit = () => {
-//                 open('hidden/sectret.html', '_self')
-//             }
-//         </script>
-//     </body>
-//     </html>`, 'https://test.me')
 
 module.exports = {
     normalizeURL,
--- crawl.test.js
+++ crawl.test.js
@@ -12,7 +12,7 @@ test('Tests trailing /', () => {
 // From Step 2
 
 test('Find straight out link', ()=>{
-    expect(getURLsFromHTML(`<a href="https://test.you/new_page.html">`,'https://test.me') === ["https://test.you/new_page.html"]).toBe(true)
+    expect(getURLsFromHTML(`<a href="https://test.you/new_page.html">`,'https://test.me').toString() === ["https://test.you/new_page.html"].toString()).toBe(true)
 })
 test('Find burried link', ()=>{
     expect(getURLsFromHTML(`<!DOCTYPE html>
@@ -25,35 +25,8 @@ test('Find burried link', ()=>{
     <body>
         <a href="http://test.you/new_page.html"></a>
     </body>
-    </html>`,'https://test.me') === ["http://test.you/new_page.html"]).toBe(true)
+    </html>`,'https://test.me').toString() === [ 'http://test.you/new_page.html' ].toString()).toBe(true)
 })
 test('Find relative url from <a>',()=>{
-    expect(getURLsFromHTML(`<a href="/new_page.html">`,'https://test.me') === ["https://test.me/new_page.html"]).toBe(true)
-})
-test('Find relative link hidden', () => {
-    expect(getURLsFromHTML(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Test</title>
-    </head>
-    <body>
-        <button type="submit" onsubmit="submit()">Click Me!</button>
-    
-        <script>
-            const submit = () => {
-                open('hidden/sectret.html', '_self')
-            }
-        </script>
-    </body>
-    </html>`, 'https://test.me') === ['https://test.me/hidden/secret.html']).toBe(true)
-})
-
-test('Get multiple url\'s from the same htmlBody',()=>{
-    expect(getURLsFromHTML(`<a href="http://google.com">google</a>
-    <script>
-        let b = document.body.getElementById('submit')
-        b.textContent = "https://bing.com"
-    </script>`,'https://test.me') === ['https://google.com', 'https://bing.com'])
+    expect(getURLsFromHTML(`<a href="/new_page.html">`,'https://test.me').toString() === [ 'https://test.me/new_page.html' ].toString()).toBe(true)
 })
