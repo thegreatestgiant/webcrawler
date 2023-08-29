88bda185f271493c02faa2d33f8007bff7d27da0
thegreatestgiant <seanalter1@gmail.com>
2023-08-28 22:46:00 -0400

    b4 chatgpt

--- crawl.js
+++ crawl.js
@@ -1,9 +1,58 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new JSDOM(htmlBody)
+    let arr = []
+    let arr_of_anchor = dom.window.document.querySelectorAll('a')
+    for (a of arr_of_anchor) {
+        let href = a.getAttribute('href');
+        if (href && href.startsWith('./')) {
+            href = href.split('/').slice(0, -1).join('/');
+        }
+        try {
+            let url = new URL(a)
+            console.log(url)
+            arr.push(url.href)
+        } catch (e) {
+            let url = new URL(baseURL + a)
+            console.log(url)
+            if(url.hostname.endsWith('.')){
+                console.log(url.hostname)
+                console.log(url)
+            }
+            arr.push(url.href)
+        }
+    }
+    console.log(arr)
+
+}
+
+
 function normalizeURL(str){
     const url = new URL(str)
     return url.hostname + url.pathname
 }
-
+getURLsFromHTML(`<a href="./new_page.html">`,'https://test.me')
+// getURLsFromHTML(`<!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>Test</title>
+//     </head>
+//     <body>
+//         <button type="submit" onsubmit="submit()">Click Me!</button>
+    
+//         <script>
+//             const submit = () => {
+//                 open('hidden/sectret.html', '_self')
+//             }
+//         </script>
+//     </body>
+//     </html>`, 'https://test.me')
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
   }
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,59 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
 
+// From Step 1
 test('Normalizes https and http', ()=>{
     expect(normalizeURL('http://google.com') === (normalizeURL('https://google.com'))).toBe(true)
 })
 test('Tests trailing /', () => {
     expect(normalizeURL('https://google.com/') === (normalizeURL('https://google.com'))).toBe(true)
+})
+
+// From Step 2
+
+test('Find straight out link', ()=>{
+    expect(getURLsFromHTML(`<a href="https://test.you/new_page.html">`,'https://test.me') === ["https://test.you/new_page.html"]).toBe(true)
+})
+test('Find burried link', ()=>{
+    expect(getURLsFromHTML(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Test</title>
+    </head>
+    <body>
+        <a href="http://test.you/new_page.html"></a>
+    </body>
+    </html>`,'https://test.me') === ["http://test.you/new_page.html"]).toBe(true)
+})
+test('Find relative url from <a>',()=>{
+    expect(getURLsFromHTML(`<a href="/new_page.html">`,'https://test.me') === ["https://test.me/new_page.html"]).toBe(true)
+})
+test('Find relative link hidden', () => {
+    expect(getURLsFromHTML(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Test</title>
+    </head>
+    <body>
+        <button type="submit" onsubmit="submit()">Click Me!</button>
+    
+        <script>
+            const submit = () => {
+                open('hidden/sectret.html', '_self')
+            }
+        </script>
+    </body>
+    </html>`, 'https://test.me') === ['https://test.me/hidden/secret.html']).toBe(true)
+})
+
+test('Get multiple url\'s from the same htmlBody',()=>{
+    expect(getURLsFromHTML(`<a href="http://google.com">google</a>
+    <script>
+        let b = document.body.getElementById('submit')
+        b.textContent = "https://bing.com"
+    </script>`,'https://test.me') === ['https://google.com', 'https://bing.com'])
 })
